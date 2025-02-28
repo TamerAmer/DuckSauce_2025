48f27b1336c4918380fb78196fba826e2feb1579
Kristian Anastasiou <kristian.anastasiou@glasgow.ac.uk>
2025-02-28 23:39:09 +0000

    Second iteration of grid

--- grid.py
+++ grid.py
@@ -23,18 +23,44 @@ class Grid():
                                   self.gridOffsetZ + r * self.gridSpaceSize,
                                   self.gridSpaceSize, self.gridSpaceSize], 1)
 
-    #def CheckMobPlacement(self, mob):
-    #    partsList = mob.Parts
-    #    for p in PartsList:
+    def CheckAlienPlacement(self, alien, chosen_coords):
+        parts_list = alien.returnParts()
+        placement_valid = True
+        for p in parts_list:
+            coords = p.gridCo
+            absolute_part_coords = coords + chosen_coords
+
+            # Check if alien parts are out of bound
+            if (absolute_part_coords < 0) or (absolute_part_coords > self.columns) or (absolute_part_coords < 0) or (absolute_part_coords > self.rows):
+                placement_valid = False
+
+            # Check if alien parts collide with other alien parts
+            for i in self.gridSpaceMetadata:
+                if (i.alien_part != None):
+                    if (i.coords == absolute_part_coords):
+                        placement_valid = False
+
+        return placement_valid
+
+    def AddAlien(self, alien, chosen_coords):
+        parts_list = alien.returnParts()
+        for p in parts_list:
+            coords = p.gridCo
+            absolute_part_coords = coords + chosen_coords
+            self.ModifyGridSpace(absolute_part_coords, p)
 
 
+    def ModifyGridSpace(self, coords, alien_part):
+        for i in self.gridSpaceMetadata:
+            if i.coords() == coords:
+                i.alien_part = alien_part
 
 
 # GridSpace is the element of a grid. It has grid coordinates and mobPart for the info of monsters
 class GridSpace():
-    def __init__(self, coords, mobPart):
+    def __init__(self, coords, alien_part):
         self.coords = coords
-        self.mobPart = mobPart
+        self.alien_part = alien_part
 
     def getCoordX(self):
         return self.coords(0)
@@ -43,7 +69,7 @@ class GridSpace():
         return self.coords(1)
 
     def getMobPart(self):
-        return self.mobPart
+        return self.alien_part
 
 
 
