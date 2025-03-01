ce152d23a63b7cbd08f4a5cbadf54bac77fe0adb
Tamer <tamer_amer30@outlook.com>
2025-03-01 13:43:16 +0000

    added spritesheet logic to fighter and crabman

--- fighters/fighters.py
+++ fighters/fighters.py
@@ -1,4 +1,14 @@
-class Fighters: 
-    def __init__(self,selected):
-        self.selected = selected
+import settings
+import pygame
 
+class Fighters(pygame.sprite.Sprite):
+    def init(self, selected, spritesheet, x, y):
+        pygame.sprite.Sprite.init(self)
+        self.selected = selected
+        self.spritesheet = spritesheet
+        self.image = pygame.Surface((10, 10))
+        self.image.fill(settings.TOMATO)  # placeholder surface and rect before the sprite is added
+        self.rect = self.image.get_rect()
+        self.original_image = self.image
+        self.x = x
+        self.y = y
