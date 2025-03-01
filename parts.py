dac39ac208bf6d8f1bde39cf89f95623087993fc
Tamer <tamer_amer30@outlook.com>
2025-03-01 17:11:45 +0000

    deleted duplicate code

--- parts.py
+++
@@ -1,9 +0,0 @@
-from battleTypes import BattleType
-
-class Parts(BattleType):
-    def __init__(self, gridCo, hp, parent, totalHp, battleType:BattleType):
-        self.hp = hp
-        self.gridCo = gridCo
-        self.parent = parent
-        self.totalHp = totalHp
-        self.BattleType = battleType
