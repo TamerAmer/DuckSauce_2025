7c5b2d3cd5aed1deb8ab054e2d5590adfb604257
Tamer <tamer_amer30@outlook.com>
2025-03-01 20:06:02 +0000

    added sprite subtexture

--- fighters/alienClasses/waspMan.py
+++ fighters/alienClasses/waspMan.py
@@ -2,6 +2,7 @@ from fighters.fighters import Fighters
 from parts import Parts
 from battleTypes import BattleType
 import pygame
+import settings
 
 class WaspMan(Fighters):
     def __init__(self, selected, spritesheet):
@@ -9,6 +10,7 @@ class WaspMan(Fighters):
         pygame.sprite.Sprite.__init__(self)
         self.original_image = spritesheet.get_image_name("waspman")
         self.image = self.original_image
+        self.image = pygame.transform.scale(self.image,(settings.GRID_SPACE_SIZE * 2, settings.GRID_SPACE_SIZE*2))
         self.rect = self.image.get_rect()
         self.shape = [[0,0],[0,1],[1,1]]
         self.parts = [Parts(2, self.shape[0],"wasp",6, BattleType.Air),
