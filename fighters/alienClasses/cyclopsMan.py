296b608a255cacd3942c5d0fde31963643e02520
Tamer <tamer_amer30@outlook.com>
2025-03-02 02:24:19 +0000

    added sprites

--- fighters/alienClasses/cyclopsMan.py
+++ fighters/alienClasses/cyclopsMan.py
@@ -2,6 +2,7 @@ from fighters.fighters import Fighters
 from parts import Parts
 from battleTypes import BattleType
 import pygame
+import settings
 
 class CyclopsMan(Fighters):
     def __init__(self, selected, spritesheet):
@@ -9,6 +10,7 @@ class CyclopsMan(Fighters):
         pygame.sprite.Sprite.__init__(self)
         self.original_image = spritesheet.get_image_name("cyclopsman")
         self.image = self.original_image  # Needed for proper rotation
+        self.image = pygame.transform.scale(self.image,(settings.GRID_SPACE_SIZE * 2, settings.GRID_SPACE_SIZE))
         self.rect = self.image.get_rect()
         self.shape = [[0,0],[0,1]]
         self.parts = [Parts(2, self.shape[0],"cyclops",4, BattleType.Land),
